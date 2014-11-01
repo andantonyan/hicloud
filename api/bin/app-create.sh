18ccd59c95f363c487539d39f81f2118a4abf176
alex <alexander.vanyan@gmail.com>
2014-11-01 22:10:29 +0400

    app-create basic shellscript

---
+++ api/bin/app-create.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+USERDIR="$1/$2"
+APPDIR="$USERDIR/$3.git"
+USER="appuser_$2"
+
+sudo mkdir -p "$APPDIR"
+sudo useradd "$USER"
+sudo usermod -d "$USERDIR" "$USER"
+sudo chown -R "$USER"."$USER" "$USERDIR"
+
+if [ ! -d "$USERDIR/.ssh" ]; then
+    sudo su - "$USER" -c 'mkdir .ssh && cd .ssh && ssh-keygen -t rsa -N "" -f id_rsa'
+fi
+
+if [ ! -f "$USERDIR/.ssh/authorized_keys" ]; then
+    sudo su - "$USER" -c "touch $USERDIR/.ssh/authorized_keys"
+fi
+
+sudo su - "$USER" -c "cd $APPDIR && git --bare init"
+sudo su - "$USER" -c "cp $1/defaults/post-update.sample $APPDIR/.git/hooks/post-update"
+
+exit 0
