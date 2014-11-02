81ed827e17e41d152b22ab2eb99714ecb41b15b8
alex <alexander.vanyan@gmail.com>
2014-11-02 15:35:54 +0400

    ssh-keys-get

--- api/services/hardwareApi.js
+++ api/services/hardwareApi.js
@@ -9,13 +9,25 @@ var hwApi = {
 };
 
 hwApi.app.create = function(userId, appName) {
-    return execCommand('app-create', 'application has been created successfully', [userId, appName]);
+    return execCommand(
+        'app-create',
+        'application has been created successfully',
+        [userId, appName]
+    );
 };
 
+hwApi.user.keys = function(userId) {
+    return execCommand(
+        'ssh-keys-get',
+        'ssh keys',
+        [userId],
+        'key'
+    );
+};
 
 //TODO: move this to helpers
 //TODO: add data passing capability
-var execCommand = function(command, successMsg, options) {
+var execCommand = function(command, successMsg, options, type) {
     var deferred = q.defer();
     var args = [cfg.binPath + '/' + command + '.sh', cfg.newAppDir];
     if ( _.isArray(options) ) {
@@ -28,9 +40,24 @@ var execCommand = function(command, successMsg, options) {
                 message: err.syscall + ' ' + err.errno
             });
         }
+        var data = {};
+        if ( ! _.isUndefined(type) ) {
+            switch (type) {
+                case 'key':
+                    data.keys = [];
+                    var rows = out.split("\n");
+                    _.forEach(rows, function(row) {
+                        var cols = row.split(" ");
+                        data.keys.push([ cols[2], cols[0] + ' ' + cols[1] ]);
+                    });
+                    break;
+                default:
+            }
+        }
         return deferred.resolve({
             status: 0,
-            message: successMsg
+            message: successMsg,
+            data: data
         });
     });
     return deferred.promise;
