fbfef65dc5c093b43a8470fe74c0dfe44a46857c
alex <alexander.vanyan@gmail.com>
2014-11-07 00:32:20 +0400

    app-create: dir fix

--- api/bin/app-create.sh
+++ api/bin/app-create.sh
@@ -8,10 +8,9 @@ sudo su - "$USER" -c "mkdir \"$APPDIR\""
 sudo su - "$USER" -c "cd $APPDIR && git --bare init"
 sed -i s#__DEPLOY_DIR__#$4#g $1/defaults/post-update.sample
 sudo su - "$USER" -c "cp $1/defaults/post-update.sample $APPDIR/hooks/post-update"
-sudo chown -R "$USER".nodejs "$APPDIR"
 sudo chmod g+rwx "$4"
-sudo su - "$USER" -c "mkdir -p \"$4/$2\" && cd \"$4/$2\""
-sudo chown "$USER".nodejs "$4/$2"
-sudo su - "$USER" -c "git clone \"$APPDIR\" \"$3\""
+sudo su - "$USER" -c "mkdir -p \"$4/$2\""
+sudo chown -R "$USER".nodejs "$APPDIR" "$4/$2"
+sudo su - "$USER" -c "cd \"$4/$2\" && git clone \"$APPDIR\" \"$3\""
 
 exit 0
