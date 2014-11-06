49ba2251ed4387a1d5d887b1cbe7ba25ca870896
alex <alexander.vanyan@gmail.com>
2014-11-07 00:33:51 +0400

    __DEPLOY_DIR__ replace fix

--- api/bin/app-create.sh
+++ api/bin/app-create.sh
@@ -6,8 +6,8 @@ USER="$2"
 
 sudo su - "$USER" -c "mkdir \"$APPDIR\""
 sudo su - "$USER" -c "cd $APPDIR && git --bare init"
-sed -i s#__DEPLOY_DIR__#$4#g $1/defaults/post-update.sample
 sudo su - "$USER" -c "cp $1/defaults/post-update.sample $APPDIR/hooks/post-update"
+sed -i s#__DEPLOY_DIR__#$4#g "$APPDIR/hooks/post-update"
 sudo chmod g+rwx "$4"
 sudo su - "$USER" -c "mkdir -p \"$4/$2\""
 sudo chown -R "$USER".nodejs "$APPDIR" "$4/$2"
