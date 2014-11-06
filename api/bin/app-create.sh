7ad61f50ded641f13d1a66e2a6522c9630f4f7ff
alex <alexander.vanyan@gmail.com>
2014-11-06 22:26:10 +0400

    git update permission fix

--- api/bin/app-create.sh
+++ api/bin/app-create.sh
@@ -9,6 +9,8 @@ sudo su - "$USER" -c "cd $APPDIR && git --bare init"
 sed -i s#__DEPLOY_DIR__#$4#g $1/defaults/post-update.sample
 sudo su - "$USER" -c "cp $1/defaults/post-update.sample $APPDIR/hooks/post-update"
 sudo chown -R "$USER".nodejs "$APPDIR"
-mkdir -p "$4/$2" && cd "$4/$2" && git clone "$APPDIR" "$3"
+mkdir -p "$4/$2" && cd "$4/$2"
+sudo chown "$USER".nodejs .
+sudo su - "$USER" -c "git clone \"$APPDIR\" \"$3\""
 
 exit 0
