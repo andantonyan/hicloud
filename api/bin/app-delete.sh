a863ea5e81a66d1ac5c5bfcbfe4a58a972ead128
alex <alexander.vanyan@gmail.com>
2014-11-08 20:59:36 +0400

    rm comment

--- api/bin/app-delete.sh
+++ api/bin/app-delete.sh
@@ -9,7 +9,6 @@ USERDIR="$APPDIR/$USER"
 USERAPPDIR="$USERDIR/$APPNAME.git"
 USERWWWDIR="$WWWDIR/$USER"
 
-# TODO: stop server, delete record in dns zone
 sudo su - "$USER" -c "rm -rf \"$USERAPPDIR\" \"$USERWWWDIR/$APPNAME\""
 
 exit 0
